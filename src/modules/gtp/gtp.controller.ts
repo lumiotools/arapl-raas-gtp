c5e3ebfef90a355cc5cdc4fd19c58368db4a2eee
rishav raj <rishav21556@iiitd.ac.in>
2025-07-01 16:15:32 +0530

    minor update in controller - gtp

--- src/modules/gtp/gtp.controller.ts
+++ src/modules/gtp/gtp.controller.ts
@@ -29,7 +29,7 @@ export class GtpController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gtpService.remove(id);
+  async remove(@Param('id') id: string) {
+    return await  this.gtpService.remove(id);
   }
 }
