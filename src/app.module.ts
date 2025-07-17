e75b2de19040aae9bfa91d9a90e425070b8f8253
rishav raj <rishav21556@iiitd.ac.in>
2025-07-17 18:42:26 +0530

    adding move_type in the task entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { TriggerModule } from './modules/trigger/trigger.module';
 import { LoggingModule } from './modules/logging/logging.module';
 import { DatabaseResetModule } from './modules/database-reset/database-reset.module';
 import { WaitingLocationModule } from './modules/waiting_location/waiting_location.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { WaitingLocationModule } from './modules/waiting_location/waiting_locati
     TriggerModule,
     DatabaseResetModule,
     WaitingLocationModule,
+    DashboardModule,
   ],
   controllers: [],
   providers: [],
