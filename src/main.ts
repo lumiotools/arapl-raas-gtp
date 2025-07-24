a54aa48aa764e3ef7bbea5ff21b10b30344a5ec6
rishav raj <rishav21556@iiitd.ac.in>
2025-07-24 20:23:28 +0530

    deployment config updated

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'https://arapl-raas-gtp-ui.vercel.app', // Added Vercel frontend URL
-      'http://localhost:3030', // Local development URL
+      // 'http://localhost:3030', // Local development URL
       'https://arapl-raas-wms-api-layer.onrender.com',
     ],
     credentials: true, // Important for cookies
