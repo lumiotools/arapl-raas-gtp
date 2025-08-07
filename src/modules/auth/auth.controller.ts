b8aaa2e1eaf3035e56a1ed4c829e2c43a101bb0f
rishav raj <rishav21556@iiitd.ac.in>
2025-08-07 16:55:36 +0530

    change to production configuration

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -27,7 +27,7 @@ export class AuthController {
       res.cookie('token', result.token, {
         httpOnly: true, // Prevents JavaScript access, more secure
         secure: process.env.NODE_ENV === 'production', // Only sent over HTTPS in production
-        sameSite: 'lax', // Allows cross-site requests (frontend/backend on different domains)
+        sameSite: 'none', // Allows cross-site requests (frontend/backend on different domains)
         maxAge: 10 * 60 * 60 * 1000 // 10 hours
       });
     }
